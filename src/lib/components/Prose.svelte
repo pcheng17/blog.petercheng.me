a9a30a1bd63455f8c53494453bd4d997e4028424
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-10-13 00:02:40 -0700

    Random update

--- src/lib/components/Prose.svelte
+++ src/lib/components/Prose.svelte
@@ -2,6 +2,6 @@
   export let content;
 </script>
 
-<div class="prose prose-base text-gray-900 prose-strong:font-bold max-w-none prose-code:font-light prose-p:my-4 prose-a:no-underline prose-a:text-penn-red hover:prose-a:underline hover:prose-a:underline-offset-2 hover:prose-a:decoration-1 prose-a:font-normal prose-code:text-[14px] prose-pre:rounded-md prose-pre:border-solid prose-pre:border-[1px] prose-pre:border-neutral-300 prose-img:mx-auto">
+<div class="prose prose-base text-gray-900 prose-strong:font-bold max-w-none prose-code:font-light prose-p:my-4 prose-a:no-underline prose-a:text-penn-red hover:prose-a:underline hover:prose-a:underline-offset-2 hover:prose-a:decoration-1 prose-a:font-normal prose-code:text-[15px] prose-pre:rounded-md prose-pre:border-solid prose-pre:border-[1px] prose-pre:border-neutral-300 prose-img:mx-auto">
   <svelte:component this={content} />
 </div>
