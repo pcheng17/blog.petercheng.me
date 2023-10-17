af313a77e236b82d7ac6a09e12dadb93dfe2bfc2
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-10-17 00:30:14 -0700

    Fix dates

--- src/routes/api/published_posts/+server.ts
+++ src/routes/api/published_posts/+server.ts
@@ -1,36 +1,26 @@
 import type { RequestHandler } from "./$types";
 import { fetchPosts } from "$lib/utils";
 import { json } from "@sveltejs/kit";
-import { DateTime, Settings } from "luxon";
-
-// Ensure that the default zone is set to 'America/Los_Angeles'.
-Settings.defaultZoneName = "America/Los_Angeles";
 
 export const GET: RequestHandler = async () => {
   const allPosts = await fetchPosts();
-  const currentDate = DateTime.now().startOf("day");
+  // const currentDate = DateTime.now().startOf("day");
+  const currentDate = new Date();
 
-  console.log("currentDate", currentDate.toISODate());
+  console.log("currentDate", currentDate);
 
   const sortedPosts = allPosts
     .filter((post) => !post.meta.draft)
     .filter((post) => {
       console.log("post.meta.pubDate", post.meta.pubDate);
-      const postDate = DateTime.fromISO(post.meta.pubDate, {
-        zone: "America/Los_Angeles",
-      });
-      console.log("postDate", postDate.toISODate());
+      const postDate = new Date(post.meta.pubDate);
       console.log("postDate <= currentDate", postDate <= currentDate);
       return postDate <= currentDate;
     })
     .sort((a, b) => {
-      const dateA = DateTime.fromISO(a.meta.pubDate, {
-        zone: "America/Los_Angeles",
-      });
-      const dateB = DateTime.fromISO(b.meta.pubDate, {
-        zone: "America/Los_Angeles",
-      });
-      return dateB.valueOf() - dateA.valueOf();
+      const dateA = new Date(a.meta.pubDate);
+      const dateB = new Date(b.meta.pubDate);
+      return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
     });
   
   console.log("sortedPosts", sortedPosts.length);
