bf4dd8b337f57cb8127f301165ff8984213018bb
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-10-17 00:19:56 -0700

    Add console log

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,6 +2,8 @@ export const load = async ({ fetch }) => {
   const response = await fetch(`/api/published_posts`);
   const posts = await response.json();
 
+  console.log("posts", posts.length);
+
   return {
     posts,
   };
--- src/routes/api/published_posts/+server.ts
+++ src/routes/api/published_posts/+server.ts
@@ -10,12 +10,17 @@ export const GET: RequestHandler = async () => {
   const allPosts = await fetchPosts();
   const currentDate = DateTime.now().startOf("day");
 
+  console.log("currentDate", currentDate.toISODate());
+
   const sortedPosts = allPosts
     .filter((post) => !post.meta.draft)
     .filter((post) => {
+      console.log("post.meta.pubDate", post.meta.pubDate);
       const postDate = DateTime.fromISO(post.meta.pubDate, {
         zone: "America/Los_Angeles",
       });
+      console.log("postDate", postDate.toISODate());
+      console.log("postDate <= currentDate", postDate <= currentDate);
       return postDate <= currentDate;
     })
     .sort((a, b) => {
@@ -27,6 +32,7 @@ export const GET: RequestHandler = async () => {
       });
       return dateB.valueOf() - dateA.valueOf();
     });
-
+  
+  console.log("sortedPosts", sortedPosts.length);
   return json(sortedPosts);
 };
