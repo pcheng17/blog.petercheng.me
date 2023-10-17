89a30efa406e550bb5a9f1444426c631ca4a149c
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-10-17 00:32:26 -0700

    Clean up console log

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,8 +2,6 @@ export const load = async ({ fetch }) => {
   const response = await fetch(`/api/published_posts`);
   const posts = await response.json();
 
-  console.log("posts", posts.length);
-
   return {
     posts,
   };
--- src/routes/api/published_posts/+server.ts
+++ src/routes/api/published_posts/+server.ts
@@ -4,17 +4,12 @@ import { json } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
   const allPosts = await fetchPosts();
-  // const currentDate = DateTime.now().startOf("day");
   const currentDate = new Date();
-
-  console.log("currentDate", currentDate);
 
   const sortedPosts = allPosts
     .filter((post) => !post.meta.draft)
     .filter((post) => {
-      console.log("post.meta.pubDate", post.meta.pubDate);
       const postDate = new Date(post.meta.pubDate);
-      console.log("postDate <= currentDate", postDate <= currentDate);
       return postDate <= currentDate;
     })
     .sort((a, b) => {
@@ -23,6 +18,5 @@ export const GET: RequestHandler = async () => {
       return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
     });
   
-  console.log("sortedPosts", sortedPosts.length);
   return json(sortedPosts);
 };
