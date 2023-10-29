18d05563712aa88eba2b04bdb1c28fe902a13d4b
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-10-29 02:34:46 -0700

    Update about me page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,31 +1,30 @@
 <script lang="ts">
   import Katex from '$lib/components/Katex.svelte';
-  import Prose from '$lib/components/Prose.svelte';
   import Title from '$lib/components/Title.svelte';
   import { siteUrl } from '$lib/config.js';
-  export let data;
 
   let url: string = [siteUrl, "about"].join("/");
-  let title: string = data.title;
+  let title: string = "About me";
+  let description: string = "A little bit about Peter Cheng";
+  let hasMath: boolean = false;
 </script>
 
 <svelte:head>
   <title>{title}</title>
   <link rel="canonical" href={url} />
   <meta name="title" content={title} />
-  <meta name="description" content={data.description} />
+  <meta name="description" content={description} />
   <meta name="robots" content="index, follow" />
   <meta property="og:title" content={title} />
-  <meta property="og:description" content={data.description} />
+  <meta property="og:description" content={description} />
   <meta property="og:type" content="article" />
   <meta property="og:url" content={url} />
 
-  <Katex hasMath={data.math} />
+  <Katex {hasMath} />
 </svelte:head>
 
 <main>
   <article>
-    <!-- <Prose content={data.content} /> -->
     <img src="/images/me.png" alt="Peter Cheng" class="float-left mr-6 my-3 aspect-square rounded-full w-40 h-40 border-2 border-gray-500"/>
     <Title {title} />
     <p class="my-4">
