8a0b5d8c6910a1d54270d74a5d14d7f6bfc4a353
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-11-02 00:13:23 -0700

    Add footer with copyright

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
+<footer class="py-4">
+  <div class="container mx-auto text-center">
+    <p class="text-xs">© {year} Peter Cheng</p>
+    <!-- <div class="space-x-4">
+      <a href="/about" class="text-gray-400 hover:text-white">About</a>
+      <a href="/contact" class="text-gray-400 hover:text-white">Contact</a>
+      <a href="/privacy" class="text-gray-400 hover:text-white">Privacy Policy</a>
+    </div> -->
+  </div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,14 @@
   import "../app.css";
   import "@fontsource/ubuntu-mono";
   import Header from "$lib/components/Header.svelte";
+  import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <div style="font-family: Typewriter-Light;" class="text-base leading-7 text-black p-4 mx-auto max-w-screen-md">
   <Header />
-  <div class="mt-6 mb-36">
+  <div class="mt-6 mb-32">
     <slot />
   </div>
+  <Footer />
 </div>
 
