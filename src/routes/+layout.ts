7b83e6d6ef8d05ede63ece822e2f4cd37d2dac1a
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-22 23:21:01 -0800

    feat: add new vercel speed insight

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,5 +3,7 @@
 
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 
 inject({ mode: dev ? 'development' : 'produdction' });
+injectSpeedInsights();
