0b96f15fc842f799c91392440abeef3e76dc406f
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-04-12 23:38:26 -0700

    Update about me

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -30,7 +30,7 @@
       I founded Ninjabyte Computing in 2019 to provide software, computational, and simulation consulting 
       services to academic research groups. I currently work part-time with the
       <a href="https://seguralab.duke.edu/" class="prose-link" target="_blank">Segura Lab</a> at Duke 
-      University, where we are developing software for analyzing the void space of packed particles. Check 
+      University, where we've developed novel software for analyzing the void space of packed particles. Check 
       out our work on <a href="https://www.nature.com/articles/s43588-023-00551-x" class="prose-link" target="_blank">Nature Computational Science</a>!
     </p>
     <p class="my-4">
@@ -39,13 +39,18 @@
       inspiration for this website!
     </p>
     <p class="my-4">
+      On the topic of academia, I am also an Adjunct Assistant Professor at South Dakota State University in 
+      the Department of Mathematics and Statistics. While I don't teach any courses, I do offer my time  
+      as an advisor for master's students who would like to work on computational projects broadly
+      with my expertise of numerical analysis and physics simulations.
+    </p>
+    <p class="my-4">
       When I'm not working, I enjoy dabbling in various projects:
     </p>
     <ul class="list-disc list-outside pl-[26px] mt-5 marker:text-[#d1d5db]">
       <li class="prose-list">
-        I'm very interested in this AI revolution that we're experiencing with GPT and related
-        technologies, so I often spend my time building small apps that allow me to experiment with
-        GPT. I would love to hear about the ideas you have!  
+        I love to tinker with code, so I tend to spend time on random little projects that catch my
+        attention. It's hard to really describe the scope of these projects...
       </li>
       <li class="prose-list">
         This website, which serves two purposes: (1) it encourages me to document things I've learned to
@@ -54,7 +59,9 @@
         <a href="https://kit.svelte.dev/" class="prose-link" target="_blank">SvelteKit</a>.  
       </li>
       <li class="prose-list">
-        I also try and keep up with my Korean studies as much as I can.
+        I try and keep up with my Korean studies as much as I can. I started learning when the pandemic
+        hit because of my interest in both the country and the language, and I've been studying ever
+        since. Sadly, I'm still far from fluent...
       </li>
     </ul>
   </article>
